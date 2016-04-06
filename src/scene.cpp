086555d59b286b765e9c6c8e97852d6899fa9fa4
Ben <i7620560@bournemouth.ac.uk>
2016-04-06 14:01:32 +0100

    Everything is crashing what is life

--- src/scene.cpp
+++ src/scene.cpp
@@ -22,72 +22,78 @@ void scene::draw(float dt)
 
 void scene::broadPhase(std::vector<actor *> _ents, std::vector<navPoint *> _p, aabb _box, short lvl)
 {
-    int count = 0;
-    partition pass;
+  int count = 0;
+  partition pass;
 
-    for(auto &i : _ents)
+  for(auto &i : _ents)
+  {
+    if(pointVSAABB(i->getPos(), _box))
     {
-        if(pointVSAABB(i->getPos(), _box))
-        {
-            count++;
-            pass.m_actors.push_back(i);
-        }
+      count++;
+      pass.m_actors.push_back(i);
     }
-    for(auto &i : _p)
+  }
+  for(auto &i : _p)
+  {
+    if(pointVSAABB(i->getPos(), _box))
     {
-        if(pointVSAABB(i->getPos(), _box))
-        {
-            count++;
-            pass.m_navs.push_back(i);
-        }
+      count++;
+      pass.m_navs.push_back(i);
     }
+  }
 
 
-    if(lvl > 10 or count < 32)
+  if(lvl > 10 or count < 32)
+  {
+    //Return
+    m_partitions.push_back(pass);
+  }
+  else
+  {
+    //Divide
+    std::vector<aabb> boxes = divide(_box);
+    for(auto &i : boxes)
     {
-        //Return
-        m_partitions.push_back(pass);
+      broadPhase(_ents, _p, i, ++lvl);
     }
-    else
-    {
-        //Divide
-        std::vector<aabb> boxes = divide(_box);
-        for(auto &i : boxes)
-        {
-            broadPhase(_ents, _p, i, ++lvl);
-        }
-    }
+  }
 }
 
 void scene::generateNavConnections(const float _threshold)
 {
-    std::vector< std::vector< navPoint * > > buckets;
-    std::vector< navPoint * > ents;
+  //for(auto &i : m_navCloud) ents.push_back(&i);
 
-    for(auto &i : m_navCloud) ents.push_back(&i);
+  //aabb box = enclose(m_navCloud);
 
-    aabb box = enclose(m_navCloud);
+  std::vector<navPoint *> initEnts;
+  for(auto &i : m_navCloud) initEnts.push_back(&i);
+  std::sort(initEnts.begin(), initEnts.end(), lessX);
+  m_tree.m_node = initEnts[initEnts.size() / 2];
 
-    std::vector<navPoint *> initEnts;
-    for(auto &i : m_navCloud) initEnts.push_back(&i);
-    std::sort(initEnts.begin(), initEnts.end(), lessX);
-    tree.m_node = initEnts[initEnts.size() / 2];
+  std::vector<navPoint *> left;
+  std::vector<navPoint *> right;
 
-    std::vector<navPoint *> left;
-    std::vector<navPoint *> right;
+  for(size_t i = 0; i < initEnts.size() / 2; ++i) left.push_back(initEnts[i]);
+  for(size_t i = initEnts.size() / 2 + 1; i < initEnts.size(); ++i) right.push_back(initEnts[i]);
 
-    for(size_t i = 0; i < initEnts.size() / 2; ++i) left.push_back(initEnts[i]);
-    for(size_t i = initEnts.size() / 2 + 1; i < initEnts.size(); ++i) right.push_back(initEnts[i]);
+  int bob = 0;
+  std::cout << "pre" << std::endl;
+  std::unique_ptr<kdtree> in (&m_tree);
+  std::cout << "pre2" << std::endl;
+  m_tree.m_children.first = genKDT(std::move(in), left, 1, &bob);
+  m_tree.m_children.second = genKDT(std::move(in), right, 1, &bob);
+  std::cout << "bob " << bob << std::endl;
+  /*for(auto &i : m_navCloud)
+  {
+    navPoint * best = nullptr;
+    std::vector<std::unique_ptr<kdtree>*> path;
+    std::unique_ptr<kdtree> input (&m_tree);
+    getNearestNavPoint(i.m_pos, input, best, &path);
+  }*/
 
-    std::cout << "pre" << std::endl;
-    std::unique_ptr<kdtree> in (&tree);
-    std::cout << "pre2" << std::endl;
-    tree.m_children.first = genKDT(std::move(in), left, 1);
-    tree.m_children.second = genKDT(std::move(in), right, 1);
-    std::cout << "post" << std::endl;
 
-    //Partition navPoints
-    /*partitionNavs(&buckets, ents, box, 0, 32, 8);
+  //Partition navPoints
+  /*partitionNavs(&buckets, ents, box, 0, 32, 8);
 
   //Sort navPoints
   for(auto &bucket : buckets)
@@ -130,72 +136,78 @@ void scene::generateNavConnections(const float _threshold)
 
 void scene::partitionNavs(std::vector< std::vector<navPoint *> > * _partitions, std::vector<navPoint *> _ents, aabb _box, int lvl, int minCount, int maxLvl)
 {
-    int count = 0;
-    std::vector<navPoint *> pass;
+  int count = 0;
+  std::vector<navPoint *> pass;
 
-    for(auto &i : _ents)
+  for(auto &i : _ents)
+  {
+    if(pointVSAABB(i->getPos(), _box))
     {
-        if(pointVSAABB(i->getPos(), _box))
-        {
-            count++;
-            pass.push_back(i);
-        }
+      count++;
+      pass.push_back(i);
     }
+  }
 
-    if(lvl > maxLvl or count < minCount)
+  if(lvl > maxLvl or count < minCount)
+  {
+    //Return
+    _partitions->push_back(pass);
+  }
+  else
+  {
+    //Divide
+    std::vector<aabb> boxes = divide(_box);
+    for(auto &i : boxes)
     {
-        //Return
-        _partitions->push_back(pass);
+      partitionNavs(_partitions, _ents, i, ++lvl, minCount, maxLvl);
     }
-    else
-    {
-        //Divide
-        std::vector<aabb> boxes = divide(_box);
-        for(auto &i : boxes)
-        {
-            partitionNavs(_partitions, _ents, i, ++lvl, minCount, maxLvl);
-        }
-    }
+  }
 }
 
 std::unique_ptr<kdtree> scene::genKDT(std::unique_ptr<kdtree> _cur, std::vector<navPoint *> _ents, int _axis)
 {
-    std::cout << "p1" << std::endl;
-    if(_axis == 0) std::sort(_ents.begin(), _ents.end(), lessX);
-    else if(_axis == 1) std::sort(_ents.begin(), _ents.end(), lessY);
-    else if(_axis == 2) std::sort(_ents.begin(), _ents.end(), lessZ);
-    std::cout << "p2" << std::endl;
-    size_t index = _ents.size() / 2;
+  //if(cur == nullptr) return nullptr;
+  std::cout << "p1" << std::endl;
+  if(_axis == 0) std::sort(_ents.begin(), _ents.end(), lessX);
+  else if(_axis == 1) std::sort(_ents.begin(), _ents.end(), lessY);
+  else if(_axis == 2) std::sort(_ents.begin(), _ents.end(), lessZ);
+  std::cout << "p2" << std::endl;
+  size_t index = _ents.size() / 2;
 
-    std::vector<navPoint *> left;
-    std::vector<navPoint *> right;
+  std::vector<navPoint *> left;
+  std::vector<navPoint *> right;
 
-    for(size_t i = 0; i < index; ++i) left.push_back(_ents[i]);
-    for(size_t i = index + 1; i < _ents.size(); ++i) right.push_back(_ents[i]);
-    std::cout << "p3" << std::endl;
-    _axis += 1;
-    if(_axis > 3) _axis = 0;
-    std::cout << "p3.5" << std::endl;
-    std::unique_ptr<kdtree> newTree (new kdtree);
-    std::cout << "p3.6 " << _ents.size() << ", " << index << std::endl;
-    //pcur->m_node = _ents[index];
-    //std::cout << "p4" << std::endl;
-    if(_ents.size() <= 1)
-    {
-        std::cout << "nullpt it!" << std::endl;
-        _cur->m_node = nullptr;
-        _cur->m_children.first = nullptr;
-        _cur->m_children.second = nullptr;
-    }
-    else
-    {
-        std::cout << "recurse it!" << std::endl;
-        _cur->m_node = _ents[index];
-        _cur->m_children.first = genKDT(std::move(newTree), left, _axis);
-        _cur->m_children.second = genKDT(std::move(newTree), right, _axis);
-    }
-    std::cout << "recursion end " << &_cur << std::endl;
-    return _cur;
+  for(size_t i = 0; i < index; ++i) left.push_back(_ents[i]);
+  for(size_t i = index + 1; i < _ents.size(); ++i) right.push_back(_ents[i]);
+  std::cout << "p3" << std::endl;
+  _axis += 1;
+  if(_axis > 3) _axis = 0;
+  std::cout << "p3.5" << std::endl;
+  std::unique_ptr<kdtree> newTree (new kdtree);
+  std::cout << "p3.6 " << _ents.size() << ", " << (newTree == nullptr) << std::endl;
+  //pcur->m_node = _ents[index];
+  //std::cout << "p4" << std::endl;
+  if(_ents.size() <= 1)
+  {
+    _cur->m_axis = -1;
+    std::cout << "nullpt it 1! " << std::endl;
+    _cur->m_node = nullptr;
+    std::cout << "nullpt it 2!" << std::endl;
+    _cur->m_children.first = nullptr;
+    std::cout << "nullpt it 3!" << std::endl;
+    _cur->m_children.second = nullptr;
+    std::cout << "nullpt end!" << std::endl;
+  }
+  else
+  {
+    _cur->m_axis = _axis;
+    std::cout << "recurse it!" << std::endl;
+    _cur->m_node = _ents[index];
+    if(left.size() > 0) _cur->m_children.first = genKDT(std::move(newTree), left, _axis, _bob);
+    if(right.size() > 0) _cur->m_children.second = genKDT(std::move(newTree), right, _axis, _bob);
+  }
+  std::cout << "recursion end " << &_cur << std::endl;
+  return _cur;
 }
 /*
 std::unique_ptr<kdtree> scene::genKDT(std::unique_ptr<kdtree> _cur, std::vector<navPoint *> _ents, int _axis)
@@ -240,86 +252,146 @@ std::unique_ptr<kdtree> scene::genKDT(std::unique_ptr<kdtree> _cur, std::vector<
 */
 void scene::addActor(const vec3 _p)
 {
-    actor a (_p);
-    navPoint * npt = &m_navCloud[rand() % m_navCloud.size()];
-    a.setTPos(npt->getPos());
-    m_actors.push_back(a);
+  actor a (_p);
+  navPoint * npt = &m_navCloud[rand() % m_navCloud.size()];
+  a.setTPos(npt->getPos());
+  m_actors.push_back(a);
 }
 
 void scene::calcPath(actor *_a, navPoint *_start, navPoint *_end)
 {
-    //Nodes to consider.
-    //Stored as follows:
-    //  {node, {cost, dist}},
-    //  parent
-    //ORDERED BY F COST
-    std::vector<
-            std::pair<
-            std::pair<navPoint*, std::array<float, 2>>,
-            navPoint*>
-            > openList;
+  //Nodes to consider.
+  //Stored as follows:
+  //  {node, {cost, dist}},
+  //  parent
+  //ORDERED BY F COST
+  std::vector<
+      std::pair<
+      std::pair<navPoint*, std::array<float, 2>>,
+      navPoint*>
+      > openList;
 
-    //Nodes to remember.
-    std::vector<navPoint *> closedList;
+  //Nodes to remember.
+  std::vector<navPoint *> closedList;
 
-    //Add the start node to the closed list.
-    closedList.push_back(_start);
+  //Add the start node to the closed list.
+  closedList.push_back(_start);
 
-    while(openList.size() > 0 and std::find(closedList.begin(), closedList.end(), _end) == closedList.end())
+  while(openList.size() > 0 and std::find(closedList.begin(), closedList.end(), _end) == closedList.end())
+  {
+    //Loop through the neighbours of the node at the end of the open list (i.e. node with the lowest f cost.)
+    for(auto &i : closedList.back()->m_neighbours)
     {
-        //Loop through the neighbours of the node at the end of the open list (i.e. node with the lowest f cost.)
-        for(auto &i : closedList.back()->m_neighbours)
+      //If the considered node is already on the closed list, skip it.
+      if(std::find(closedList.begin(), closedList.end(), i) != closedList.end()) continue;
+
+      bool alreadyOnOpenList = false;
+      auto entry = openList.begin();
+      for(auto j = openList.begin(); j != openList.end(); ++j)
+      {
+        if(j->first.first == i)
         {
-            //If the considered node is already on the closed list, skip it.
-            if(std::find(closedList.begin(), closedList.end(), i) != closedList.end()) continue;
-
-            bool alreadyOnOpenList = false;
-            auto entry = openList.begin();
-            for(auto j = openList.begin(); j != openList.end(); ++j)
-            {
-                if(j->first.first == i)
-                {
-                    entry = j;
-                    alreadyOnOpenList = true;
-                }
-            }
-
-            if(!alreadyOnOpenList)
-            {
-                //If the neigbour is NOT on the open list, insert in the correct place.
-                float cost = mag(i->getPos() - closedList.back()->getPos());
-                float dist = mag(i->getPos() - _end->getPos());
-
-                bool addAtEnd = true;
-                //For each node in the open list, if it beats the current f cost, insert current node just before, then break.
-                for(auto k = openList.begin(); k != openList.end(); ++k)
-                {
-                    if(k->first.second[0] + k->first.second[1] < cost + dist)
-                    {
-                        //Insert at k a pair, containing:
-                        //  Another pair, containing:
-                        //    The current neighbour, and an array, containing:
-                        //      The cost, and the dist.
-                        //  And the parent.
-                        openList.insert(k, {{i,{cost,dist}},closedList.back()});
-                        addAtEnd = false;
-                        break;
-                    }
-                }
-                //If the f cost has NOT been beaten, just stick it on the end of the open list.
-                if(addAtEnd) openList.push_back({{i,{cost,dist}},closedList.back()});
-            }
-            else
-            {
-                //If the neighbour is on the open list, evaluate its dist vs the current square.
-                float dist = mag(closedList.back()->getPos() - _end->getPos());
-                if(dist > entry->first.second[1])
-                {
-                    entry->second = closedList.back();
-                }
-            }
+          entry = j;
+          alreadyOnOpenList = true;
         }
-        closedList.push_back(openList.back().first.first);
-        openList.pop_back();
+      }
+
+      if(!alreadyOnOpenList)
+      {
+        //If the neigbour is NOT on the open list, insert in the correct place.
+        float cost = mag(i->getPos() - closedList.back()->getPos());
+        float dist = mag(i->getPos() - _end->getPos());
+
+        bool addAtEnd = true;
+        //For each node in the open list, if it beats the current f cost, insert current node just before, then break.
+        for(auto k = openList.begin(); k != openList.end(); ++k)
+        {
+          if(k->first.second[0] + k->first.second[1] < cost + dist)
+          {
+            //Insert at k a pair, containing:
+            //  Another pair, containing:
+            //    The current neighbour, and an array, containing:
+            //      The cost, and the dist.
+            //  And the parent.
+            openList.insert(k, {{i,{cost,dist}},closedList.back()});
+            addAtEnd = false;
+            break;
+          }
+        }
+        //If the f cost has NOT been beaten, just stick it on the end of the open list.
+        if(addAtEnd) openList.push_back({{i,{cost,dist}},closedList.back()});
+      }
+      else
+      {
+        //If the neighbour is on the open list, evaluate its dist vs the current square.
+        float dist = mag(closedList.back()->getPos() - _end->getPos());
+        if(dist > entry->first.second[1])
+        {
+          entry->second = closedList.back();
+        }
+      }
     }
+    closedList.push_back(openList.back().first.first);
+    openList.pop_back();
+  }
+}
+
+void scene::getNearestNavPoint(vec3 _p, std::unique_ptr<kdtree>& _inputNode, navPoint * _best, std::vector<std::unique_ptr<kdtree>*>* _path)
+{
+  bool goLeft = true;
+  if(_inputNode->m_axis == 0)
+  {
+    if(_p.m_x > _inputNode->m_node->m_pos.m_x)
+    {
+      goLeft = false;
+    }
+  }
+  else if(_inputNode->m_axis == 0)
+  {
+    if(_p.m_y > _inputNode->m_node->m_pos.m_y)
+    {
+      goLeft = false;
+    }
+  }
+  else if(_inputNode->m_axis == 0)
+  {
+    if(_p.m_z > _inputNode->m_node->m_pos.m_z)
+    {
+      goLeft = false;
+    }
+  }
+  std::cout << "p1 " << (_inputNode == nullptr) << std::endl;
+  _path->push_back(&_inputNode);
+  std::cout << "p2 " << (_inputNode->m_children.first == nullptr) << ", " << (_inputNode->m_children.second == nullptr) << std::endl;
+
+  if(goLeft)
+  {
+    std::cout << "go left" << std::endl;
+    std::cout << "_p " << _p.m_x << ", " << _p.m_y << ", " << _p.m_z << std::endl;
+    std::cout << "_in " << &_inputNode << ", " << (_inputNode == nullptr) << std::endl;
+    std::cout << "_best " << _best << ", " << (_best == nullptr) << std::endl;
+    std::cout << "_path " << _path << std::endl;
+    if(_inputNode->m_children.first == nullptr)
+    {
+      _best = _inputNode->m_node;
+      std::cout << "bail" << std::endl;
+      return;
+    }
+    getNearestNavPoint(_p, _inputNode->m_children.first, _best, _path);
+  }
+  else
+  {
+    std::cout << "go right" << std::endl;
+    std::cout << "_p " << _p.m_x << ", " << _p.m_y << ", " << _p.m_z << std::endl;
+    std::cout << "_in " << &_inputNode << ", " << (_inputNode == nullptr) << std::endl;
+    std::cout << "_best " << _best << ", " << (_best == nullptr) << std::endl;
+    std::cout << "_path " << _path << std::endl;
+    if(_inputNode->m_children.second == nullptr)
+    {
+      _best = _inputNode->m_node;
+      std::cout << "bail" << std::endl;
+      return;
+    }
+    getNearestNavPoint(_p, _inputNode->m_children.second, _best, _path);
+  }
 }
